d0d49e00378497626874bcfa67cf1edad5529d4c
Usman972002 <usmanfaruq21@gmail.com>
2022-12-28 16:14:44 +0530

    products

--- Views/Admin/Products.aspx
+++ Views/Admin/Products.aspx
@@ -10,54 +10,89 @@
         <div class="col-md-12">
             <h3>Add Product </h3>
             <div class="row">
+
+              <div class="col-md-4 mb-2">
+                <label>Product Code</label>
+                <input type="text" placeholder="Enter Product Code"  id="Pcode" runat="server" class="form-control mt-1" />
+            </div>
+
             <div class="col-md-4 mb-2">
                 <label>Product Name</label>
-                <input type="text" placeholder="Enter Supplier Name" runat="server" class="form-control mt-1" />
+                <input type="text" placeholder="Enter Product Name"  id="Pname" runat="server" class="form-control mt-1" />
             </div>
 
             <div class="col-md-4 mb-2">
                 <label>Product Supplier </label>
-                <asp:DropDownList runat="server" class="form-control mt-1">
+                <asp:DropDownList runat="server"  id="Psupplier" class="form-control mt-1">
                     
                 </asp:DropDownList>
                 
             </div>
 
-            <div class="col-md-4 mb-2">
-                <label>Product Quantity</label>
-                <input type="number" placeholder="Quantity Of the Product" runat="server" class="form-control mt-1" />
+                <div class="col-md-4 mb-2">
+                <label>Product Category</label>
+                <asp:DropDownList runat="server" name="Pcat" id="Pcategory" class="form-control mt-1">
+                    
+                </asp:DropDownList>
             </div>
 
-
-            <div class="col-md-4 mb-2">
+             <div class="col-md-4 mb-2">
                 <label>Product Units</label>
-                <asp:DropDownList runat="server" class="form-control mt-1">
+                <asp:DropDownList runat="server" id="Punits" class="form-control mt-1">
                     
                 </asp:DropDownList>
             </div>
 
-                      
+              <div class="col-md-4 mb-2">
+                <label>Product Price</label>
+                <input type="number" placeholder="Enter product prize"  id="Pprize" runat="server" class="form-control mt-1" />
+            </div>
+                
+            <div class="col-md-4 mb-2">
+                <label>Add Date and Time </label>
+                <input type="datetime-local" id="Pdate" runat ="server" class="form-control mt-1" />
+            </div>                    
             </div>
 
-            <div class="row">
+          <div class="row mb-4">
+                
                 <div class="col-sm-1">
-                    <asp:Button Text="Edit"  class="btn btn-block btn-dark " runat="server"/>
+                    <asp:Button Text="Edit" ID="EditBtn" class="btn  btn-dark btn-block" runat="server" OnClick="EditBtn_Click" />
                 </div>
                 <div class="col-sm-1">
-                    <asp:Button Text="Save"  class="btn btn-block btn-dark" runat="server"/>
+                    <asp:Button Text="Save" ID="SaveBtn" class="btn  btn-dark btn-block" runat="server" OnClick="SaveBtn_Click" />
                 </div>
                 <div class="col-sm-1">
-                    <asp:Button Text="Delete"  class="btn btn-block btn-dark" runat="server"/>
+                    <asp:Button Text="Delete" ID="DeleteBtn" class="btn btn-dark btn-block" runat="server" OnClick="DeleteBtn_Click" />
+                </div>
+                <div class="col-sm-2">
+                    <input type="text" placeholder="Enter Id To search " id="Psearch" runat="server" class="form-control mt-1" />
+                </div>
+                <div class="col-sm-2">
+                    <input type="text" placeholder="Enter Name To search " id="Pdb1" runat="server" class="form-control mt-1" />
+                </div>
+                <div class="col-sm-1">
+                    <asp:Button Text="Search" ID="SearchBtn" class ="btn btn-dark btn-block" runat="server" OnClick="SearchBtn_Click"/>
+                </div>
+                <div class="col-sm-2">
+                    <input type="text" placeholder="Enter Id To search " id="Pdb" runat="server" class="form-control mt-1" />
                 </div>
                 
+                <div class="col-sm-1 mb-2">
+                    <asp:Button Text="GetDetails" ID="GetBtn" class ="btn btn-dark btn-block" runat="server" OnClick="GetBtn_Click"/>
+                </div>
+                <label class="text-danger text-center" id="ErrMsg" runat="server"></label>
+                
             </div>
+
+ 
 
         </div>
         </div>
         <div class="row mt-2" style="height:20px;background-color:aquamarine;"></div>
         <div class="col">
             <h3>Product's List</h3>
-            <asp:GridView runat="server"  class="table" >
+            <asp:GridView runat="server"  id="Plist" class="table" >
 
             </asp:GridView>
         </div>
--- Views/Admin/Products.aspx.cs
+++ Views/Admin/Products.aspx.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Policy;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Linq;
 
 namespace InVentory_Management_System_MarsTrackTech.Views.Admin
 {
@@ -11,7 +18,118 @@ namespace InVentory_Management_System_MarsTrackTech.Views.Admin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!IsPostBack)
+            {
+                loadCategory();
+                loadSuppliers();
+                LoadRecord();
+            }
+           
+        }
+
+     
+        SqlConnection con = new SqlConnection("Data Source = 132.148.75.0,1435; Initial Catalog = TRADING; Persist Security Info = True; User ID = amc; Password = amc123");
+        DateTime dt = DateTime.Now;
+        string format = "yyyy-MM-dd HH:mm:ss";
+
+        // To Display all the Categories
+        public void loadCategory()
+        {
+            Pcategory.Items.Clear();
+            SqlCommand command = new SqlCommand("select OM_ITEM_CATEGORY_DESCRIPTION from OM_ITEM_CATEGORY", con);
+            con.Open();
+            SqlDataReader dr = command.ExecuteReader();
+            while (dr.Read())
+            {
+                Pcategory.Items.Add(dr[0].ToString());
+            }
+            dr.Close();
+            con.Close();
 
         }
+
+        // To Display all the Suppliers
+        public void loadSuppliers()
+        {
+            Psupplier.Items.Clear();
+            SqlCommand command = new SqlCommand("select FM_VEND_NAME from FM_VENDOR_MASTER", con);
+            con.Open();
+            SqlDataReader dr = command.ExecuteReader();
+            while (dr.Read())
+            {
+                Psupplier.Items.Add(dr[0].ToString());
+            }
+            dr.Close();
+            con.Close();
+        }
+
+        void LoadRecord()
+        {
+            SqlCommand command = new SqlCommand("select OM_ITEM_CODE,OM_ITEM_MAIN_NAME,OM_ITEM_CATEGORY,FM_VEND_NAME,OM_ITEM_COST_PRICE,OM_ITEM_CREATED_DATE from FM_VENDOR_MASTER,OM_ITEM_MASTER ", con);
+            SqlDataAdapter d = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            d.Fill(dt);
+            Plist.DataSource = dt;
+            Plist.DataBind();
+        }
+
+        protected void SaveBtn_Click(object sender, EventArgs e)
+        {
+            SqlCommand com = new SqlCommand("INSERT INTO OM_ITEM_MASTER(OM_ITEM_CODE,OM_ITEM_MAIN_NAME,OM_ITEM_CATEGORY,OM_ITEM_CREATED_BY,OM_ITEM_COST_PRICE,OM_ITEM_CREATED_DATE) VALUES('" + Pcode.Value + "', '" + Pname.Value + "','" + Pcategory.Text + "','" + Psupplier.Text + "','" + Pprize.Value + "','" + dt.ToString(format) + "')", con);
+            con.Open();
+            com.ExecuteNonQuery();
+            con.Close();
+            ErrMsg.InnerText = "Product Added Succesfully!!";
+            LoadRecord();
+        }
+
+        protected void EditBtn_Click(object sender, EventArgs e)
+        {
+            SqlCommand com = new SqlCommand("UPDATE OM_ITEM_MASTER SET  OM_ITEM_MAIN_NAME = '" + Pname.Value + "',OM_ITEM_CATEGORY='" + Pcategory.Text + "',OM_ITEM_CREATED_BY='" + Psupplier.Text + "',OM_ITEM_COST_PRICE='" + Pprize.Value + "',OM_ITEM_CREATED_DATE='" + dt.ToString(format) + "' WHERE  OM_ITEM_CODE='" + Pcode.Value + "'", con);
+            con.Open();
+            com.ExecuteNonQuery();
+            con.Close();
+            ErrMsg.InnerText = "Product Updated Succesfully";
+            LoadRecord();
+        }
+
+        protected void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            SqlCommand com = new SqlCommand("DELETE FROM OM_ITEM_MASTER  WHERE  OM_ITEM_CODE='" + Pcode.Value + "'", con);
+            con.Open();
+            com.ExecuteNonQuery();
+            con.Close();
+            ErrMsg.InnerText = "Product Deleted Succesfully";
+            LoadRecord();
+        }
+
+        protected void SearchBtn_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand("select OM_ITEM_CODE,OM_ITEM_MAIN_NAME,OM_ITEM_CATEGORY,OM_ITEM_CREATED_BY,OM_ITEM_COST_PRICE,OM_ITEM_CREATED_DATE from OM_ITEM_MASTER WHERE OM_ITEM_CODE='" + Psearch.Value + "' OR OM_ITEM_MAIN_NAME='" + Pdb1.Value + "'", con);
+            SqlDataAdapter d = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            d.Fill(dt);
+            Plist.DataSource = dt;
+            Plist.DataBind();
+        }
+
+        protected void GetBtn_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand command = new SqlCommand("select OM_ITEM_CODE,OM_ITEM_MAIN_NAME,OM_ITEM_CATEGORY,OM_ITEM_CREATED_BY,OM_ITEM_COST_PRICE,OM_ITEM_CREATED_DATE from OM_ITEM_MASTER WHERE OM_ITEM_CODE='" + Pdb.Value + "'", con);
+            SqlDataReader r = command.ExecuteReader();
+            while (r.Read())
+
+            {
+                Pcode.Value = r.GetValue(0).ToString();
+                Pname.Value= r.GetValue(1).ToString();
+                Pcategory.Text= r.GetValue(2).ToString();
+                Psupplier.Text= r.GetValue(3).ToString();
+                Pprize.Value= r.GetValue(4).ToString();
+                Pdate.Value= r.GetValue(5).ToString();
+            }
+        }
     }
+
+
 }
